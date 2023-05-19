a2991db8329ed708c060c1dfcd24055fc2daba1e
Jaydip Patel <45895007+jaydippatel83@users.noreply.github.com>
2023-05-19 14:17:06 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require('axios');
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
+router.get('/hello', (req, res) => {
   res.send('Hey this is my API running 🥳')
 }) 
  
